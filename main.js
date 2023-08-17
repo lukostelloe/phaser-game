29f0857d3adcb2186fbc1e407162ebcc86240252
Luke Costelloe <lukostelloe@gmail.com>
2023-08-17 15:29:02 +0200

    add working heart pickup, add superZombie

--- main.js
+++ main.js
@@ -15,16 +15,28 @@ const game = new Phaser.Game(config);
 let player; // Reference to the blue player sprite
 let bullet; // Reference to the bullet sprite
 let hearts = [];
-let zombie; // Reference to the zombie sprite
 let zombies = [];
+let superZombies = [];
+let superZombieHits = 0;
+let superZombieRespawnDelay = 5000;
+let killcountText;
 let killcount = 0;
 const numZombies = 3;
-
+const numSuperZombies = 1;
+let heartPickup;
 let moveYellowSquare = false; // Flag to control continuous movement of the yellow square
 let bulletDirection; // Store the initial bullet direction
 let bulletMoving = false; // Flag to indicate if the bullet is moving
-
 let lastCursorKey; // Track the last cursor key pressed
+
+// Function to toggle the color of the circle between black and orange
+function toggleCircleColor(circle) {
+  if (circle.fillColor === 0xffa500) {
+    circle.fillColor = 0x000000; // Set to black
+  } else {
+    circle.fillColor = 0xffa500; // Set to orange
+  }
+}
 
 function create() {
   // Create a black background for the canvas
@@ -38,9 +50,18 @@ function create() {
   // Create the blue player sprite
   player = this.add.rectangle(400, 300, 25, 25, 0x0000ff); // Blue color
   bullet = this.add.rectangle(player.x, player.y, 10, 10, 0xffff00); // Yellow color
-  hearts.push(this.add.rectangle(25, 25, 10, 10, 0xff0000));
-  hearts.push(this.add.rectangle(40, 25, 10, 10, 0xff0000));
-  hearts.push(this.add.rectangle(55, 25, 10, 10, 0xff0000));
+
+  const heart1 = this.add.rectangle(25, 25, 10, 10, 0xff0000);
+  const heart2 = this.add.rectangle(40, 25, 10, 10, 0xff0000);
+  const heart3 = this.add.rectangle(55, 25, 10, 10, 0xff0000);
+  const heart4 = this.add.rectangle(70, 25, 10, 10, 0xff0000);
+  const heart5 = this.add.rectangle(85, 25, 10, 10, 0xff0000);
+
+  hearts.push(heart1);
+  hearts.push(heart2);
+  hearts.push(heart3);
+  hearts.push(heart4);
+  hearts.push(heart5);
 
   // Create multiple zombie sprites and add them to the zombies array
   for (let i = 0; i < numZombies; i++) {
@@ -65,11 +86,85 @@ function create() {
     zombies.push(newZombie);
   }
 
+  // Create mulitple superZombie sprites and add them to the zombies array
+  for (let i = 0; i < numSuperZombies; i++) {
+    const randomEdge = Math.floor(Math.random() * 4);
+    let superZombieX, superZombieY;
+
+    if (randomEdge === 0) {
+      superZombieX = Math.random() * config.width;
+      superZombieY = 0;
+    } else if (randomEdge === 1) {
+      superZombieX = config.width;
+      superZombieY = Math.random() * config.height;
+    } else if (randomEdge === 2) {
+      superZombieX = Math.random() * config.width;
+      superZombieY = config.height;
+    } else {
+      superZombieX = 0;
+      superZombieY = Math.random() * config.height;
+    }
+
+    const newSuperZombie = this.add.rectangle(
+      superZombieX,
+      superZombieY,
+      40,
+      40,
+      0xffa500
+    );
+    superZombies.push(newSuperZombie);
+  }
+
   // Create a text object to display the killcount
-  this.killcountText = this.add.text(25, 50, `killcount: ${killcount}`, {
+  killcountText = this.add.text(25, 50, `killcount: ${killcount}`, {
     fontFamily: "Arial",
     fontSize: 20,
     color: "#ffffff",
+  });
+
+  // Delay for 5 seconds (5000 milliseconds)
+  const delayDuration = 3000;
+  this.time.delayedCall(delayDuration, () => {
+    const minX = 100;
+    const maxX = config.width - 150;
+    const minY = 100;
+    const maxY = config.height - 150;
+
+    const randomX = Phaser.Math.Between(minX, maxX);
+    const randomY = Phaser.Math.Between(minY, maxY);
+    // Create the heart pickup and red square as a group
+    heartPickup = this.add.group();
+
+    // Create the initial orange circle in the middle of the canvas
+    const orangeCircle = this.add.circle(
+      randomX,
+      randomY,
+      15, // Radius of the circle
+      0xffa500 // Fill color in hexadecimal
+    );
+
+    // Set up the circle to flash between black and orange
+    const flashInterval = 200; // 200 milliseconds between each color change
+    this.time.addEvent({
+      delay: flashInterval,
+      loop: true,
+      callback: () => {
+        toggleCircleColor(orangeCircle);
+      },
+    });
+
+    // Create a red square in the middle of the orange circle
+    const redSquare = this.add.rectangle(
+      orangeCircle.x,
+      orangeCircle.y,
+      10, // Width of the square
+      10, // Height of the square
+      0xff0000 // Red color
+    );
+
+    // Add both the circle and square as children of the heartPickup group
+    heartPickup.add(orangeCircle);
+    heartPickup.add(redSquare);
   });
 }
 
@@ -169,12 +264,29 @@ function update() {
     // Move the zombie towards the player
     const zombieSpeed = 1; // Adjust the speed as needed
 
-    zombie.x += zombieDirectionX * zombieSpeed;
-    zombie.y += zombieDirectionY * zombieSpeed;
+    if (!zombie.isFrozen) {
+      zombie.x += zombieDirectionX * zombieSpeed;
+      zombie.y += zombieDirectionY * zombieSpeed;
+    }
 
     // Check for collision between bullet and zombie
     if (checkCollision(bullet, zombie)) {
-      respawnZombie(zombie);
+      // Stop zombie movement temporarily
+      zombie.isFrozen = true;
+      zombie.setFillStyle(0xff0000);
+
+      // Delay for a brief moment (in milliseconds)
+      const delayDuration = 800; // 1 second delay
+      this.time.delayedCall(delayDuration, () => {
+        // After the delay, respawn the zombie and resume movement
+        respawnZombie(zombie);
+        zombie.isFrozen = false;
+        zombie.setFillStyle(0x00ff00);
+      });
+
+      // Increment the killcount and update the text
+      killcount++;
+      killcountText.setText(`killcount: ${killcount}`);
     }
 
     // Check for collision between player and zombie
@@ -183,38 +295,85 @@ function update() {
       handleZombiePlayerCollision(zombie);
     }
   });
+
+  // Move each superZombie towards the player
+  superZombies.forEach((superZombie) => {
+    let superZombieDirectionX = player.x - superZombie.x;
+    let superZombieDirectionY = player.y - superZombie.y;
+
+    // Normalize the direction vector
+    const length = Math.sqrt(
+      superZombieDirectionX * superZombieDirectionX +
+        superZombieDirectionY * superZombieDirectionY
+    );
+    superZombieDirectionX /= length;
+    superZombieDirectionY /= length;
+
+    // Move the zombie towards the player
+    const superZombieSpeed = 0.5; // Adjust the speed as needed
+
+    if (!superZombie.isFrozen) {
+      superZombie.x += superZombieDirectionX * superZombieSpeed;
+      superZombie.y += superZombieDirectionY * superZombieSpeed;
+    }
+
+    // Check for collision between bullet and super zombie
+    if (checkCollision(bullet, superZombie)) {
+      // Stop zombie movement temporarily
+      superZombie.isFrozen = true;
+      superZombie.setFillStyle(0xff0000);
+
+      // Delay for a brief moment (in milliseconds)
+      const delayDuration = 800; // 1 second delay
+      this.time.delayedCall(delayDuration, () => {
+        // respawnSuperZombie(superZombie);
+        superZombieHits++;
+        if (superZombieHits > 9) {
+          superZombie.destroy();
+          superZombieHits = 0;
+        }
+
+        superZombie.isFrozen = false;
+        superZombie.setFillStyle(0xffa500);
+      });
+
+      // Increment the killcount and update the text
+      killcount++;
+      killcountText.setText(`killcount: ${killcount}`);
+    }
+
+    // Check for collision between player and zombie
+    if (checkCollision(player, superZombie)) {
+      respawnSuperZombie(superZombie);
+      handleZombiePlayerCollision(superZombie);
+    }
+  });
+
+  // Check for collision between player and heart pickup
+  if (heartPickup && heartPickup.getChildren().length > 0) {
+    if (checkGroupCollision(player, heartPickup)) {
+      if (hearts.length < 5) {
+        // Add a new heart to the list of hearts
+        const newHeart = this.add.rectangle(
+          25 + hearts.length * 15,
+          25,
+          10,
+          10,
+          0xff0000
+        );
+        hearts.push(newHeart);
+
+        // Remove the heart pickup group
+        heartPickup.destroy(true);
+        heartPickup = null;
+
+        // Respawn the heart pickup after 5 seconds
+        const respawnDelay = 5000; // 5 seconds in milliseconds
+        this.time.delayedCall(respawnDelay, respawnHeartPickup, [], this);
+      }
+    }
+  }
 }
-
-// Move each zombie towards the player
-zombies.forEach((zombie) => {
-  let zombieDirectionX = player.x - zombie.x;
-  let zombieDirectionY = player.y - zombie.y;
-
-  // Normalize the direction vector
-  const length = Math.sqrt(
-    zombieDirectionX * zombieDirectionX + zombieDirectionY * zombieDirectionY
-  );
-  zombieDirectionX /= length;
-  zombieDirectionY /= length;
-
-  // Move the zombie towards the player
-  const zombieSpeed = 1; // Adjust the speed as needed
-
-  zombie.x += zombieDirectionX * zombieSpeed;
-  zombie.y += zombieDirectionY * zombieSpeed;
-  // Check for collision between bullet and zombie
-  if (checkCollision(bullet, zombie)) {
-    respawnZombie(zombie);
-    killcount++;
-    this.killcountText.setText(`killcount: ${killcount}`);
-  }
-
-  // Check for collision between player and zombie
-  if (checkCollision(player, zombie)) {
-    respawnZombie();
-    handleZombiePlayerCollision();
-  }
-});
 
 // When a collision between the zombie and player occurs
 function handleZombiePlayerCollision() {
@@ -223,6 +382,18 @@ function handleZombiePlayerCollision() {
   if (removedHeart) {
     removedHeart.visible = false;
   }
+}
+
+function checkGroupCollision(rectangle, group) {
+  let collisionDetected = false;
+
+  group.getChildren().forEach((child) => {
+    if (checkCollision(rectangle, child)) {
+      collisionDetected = true;
+    }
+  });
+
+  return collisionDetected;
 }
 
 function checkCollision(rect1, rect2) {
@@ -256,4 +427,71 @@ function respawnZombie(zombieToRespawn) {
 
     zombies[zombieIndex].setPosition(zombieX, zombieY);
   }
+}
+
+function respawnSuperZombie(superZombieToRespawn) {
+  const superZombieIndex = superZombies.indexOf(superZombieToRespawn);
+  if (superZombieIndex !== -1) {
+    const randomEdge = Math.floor(Math.random() * 4);
+    let superZombieX, superZombieY;
+
+    if (randomEdge === 0) {
+      superZombieX = Math.random() * config.width;
+      superZombieY = 0;
+    } else if (randomEdge === 1) {
+      superZombieX = config.width;
+      superZombieY = Math.random() * config.height;
+    } else if (randomEdge === 2) {
+      superZombieX = Math.random() * config.width;
+      superZombieY = config.height;
+    } else {
+      superZombieX = 0;
+      superZombieY = Math.random() * config.height;
+    }
+
+    superZombies[superZombieIndex].setPosition(superZombieX, superZombieY);
+  }
+}
+
+function respawnHeartPickup() {
+  const minX = 100;
+  const maxX = config.width - 150;
+  const minY = 100;
+  const maxY = config.height - 150;
+
+  const randomX = Phaser.Math.Between(minX, maxX);
+  const randomY = Phaser.Math.Between(minY, maxY);
+  // Create the heart pickup and red square as a group
+  heartPickup = this.add.group();
+
+  // Create the initial orange circle in the middle of the canvas
+  const orangeCircle = this.add.circle(
+    randomX,
+    randomY,
+    15, // Radius of the circle
+    0xffa500 // Fill color in hexadecimal
+  );
+
+  // Set up the circle to flash between black and orange
+  const flashInterval = 200; // 200 milliseconds between each color change
+  this.time.addEvent({
+    delay: flashInterval,
+    loop: true,
+    callback: () => {
+      toggleCircleColor(orangeCircle);
+    },
+  });
+
+  // Create a red square in the middle of the orange circle
+  const redSquare = this.add.rectangle(
+    orangeCircle.x,
+    orangeCircle.y,
+    10, // Width of the square
+    10, // Height of the square
+    0xff0000 // Red color
+  );
+
+  // Add both the circle and square as children of the heartPickup group
+  heartPickup.add(orangeCircle);
+  heartPickup.add(redSquare);
 }
